24ec29ccbff502d5567dffdd62347fe503fd6eb2
Mudasir Habib <mudasir.ee@hotmail.com>
2022-05-09 14:40:28 +0500

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#F5F5F5",
+          200: "#EFEFEF",
+          300: "#E0E0E0",
+          400: "#D1D1D1",
+          500: "#C2C2C2",
+          600: "#B3B3B3",
+          700: "#A4A4A4",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
